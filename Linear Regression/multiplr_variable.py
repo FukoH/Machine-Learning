abcab63b2701f18656599bec754d631016d238cd
huangshengxin <huangshengxin@yonyou.com>
2017-05-08 15:20:00 +0800

    multiple variable

--- Linear Regression/multiplr_variable.py
+++ Linear Regression/multiplr_variable.py
@@ -2,6 +2,7 @@
 
 from __future__ import division
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def load_data(path):
@@ -10,6 +11,17 @@ def load_data(path):
     x2 = data[:,1]
     y = data[:,2]
     return (x1,x2,y)
+
+def load():
+    pathx = r'C:\Users\Administrator\PycharmProjects\Machine-Learning\dataset\linear regression II\ex3x.txt'
+    pathy = r'C:\Users\Administrator\PycharmProjects\Machine-Learning\dataset\linear regression II\ex3y.txt'
+    X = np.genfromtxt(pathx)
+    Y = np.genfromtxt(pathy)
+    x1 = X[:,0]
+    x2 = X[:,1]
+    Y = Y.reshape((len(Y)), 1)
+    #X = np.column_stack((X, np.ones_like(Y)))
+    return x1,x2,Y
 
 def cost_function(theta, x, y):
     m = len(y)
@@ -29,17 +41,38 @@ def batch_gradient_descent(alpha, theta, x, y,num_iters):
     return theta,J_history
 
 
-def test():
-    x1,x2,y = load_data("D:\machine learning\linear regression\ex1data2.txt")
-    x = np.column_stack(((x1 - np.mean(x1))/np.std(x1),(x2 - np.mean(x2))/np.std(x2),np.ones_like(y)))
-    y = (y - np.mean(y))/np.std(y)
-    y = np.array(y).reshape((len(y),1))
-    iteration = 1500
-    alpha = 0.001
-    theta = np.zeros((3,1))
-    theta,J_history= batch_gradient_descent(alpha,theta,x,y,iteration)
-    print theta
-    print J_history
 
-if __name__=='__main__':
-    test()
+x1,x2,y = load()
+#X = np.column_stack((x1,x2))
+#mean = np.mean(X)
+f_x1 = (x1 - np.mean(x1))/np.std(x1)
+f_x2 = (x2 - np.mean(x2))/np.std(x2)
+x = np.column_stack((np.ones_like(y),f_x1,f_x2))
+#y = (y - np.mean(y))/np.std(y)
+#y = np.array(y).reshape((len(y),1))
+iteration = 100
+alphas = [0.01,0.03,0.1,0.3,1,1.3]
+plotstyle = ['r','b','k','g','r--','b--']
+#theta = np.zeros((3,1))
+
+#theta,J_history= batch_gradient_descent(alpha,theta,x,y,iteration)
+converge_theta = None
+theta = np.zeros((3, 1))
+for i in range(len(alphas)):
+    result_theta, J_history =batch_gradient_descent(alphas[i], theta, x, y, iteration)
+    plt.plot(range(50),J_history[0:50],plotstyle[i])
+    if(alphas[i]==1):
+        converge_theta = result_theta
+plt.legend(['0.01','0.03','0.1', '0.3', '1', '1.3'])
+plt.xlabel("number of iterations")
+plt.ylabel("cost J")
+
+print converge_theta
+#print J_history
+plt.show()
+
+#make prediction
+predict = np.array((1,(1650-np.mean(x1))/np.std(x1),(3-np.mean(x2))/np.std(x2)))
+result = predict.dot(converge_theta)
+print result
+
